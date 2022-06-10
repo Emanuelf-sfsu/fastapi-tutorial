cf4d606273005a33508303e96b3a0cf7070ea3dd
emanuelf-sfsu <efrancis1@mail.sfsu.edu>
2022-06-10 14:43:49 -0700

    adding number parameters

--- myapi.py
+++ myapi.py
@@ -1,7 +1,28 @@
-from fastapi import FastAPI
+# Stopped at 27:11
+# https://www.youtube.com/watch?v=tLKKmouUams
+from fastapi import FastAPI, Path
 
 app = FastAPI()
+# Example data 
+students = {
+    1: {
+        "name":"john",
+        "age" : 22,
+        "class" : "year 2"
+    }
+}
 
+#home
 @app.get("/")
 def index():
     return {"name" : "First Data"}
+
+#Searching student
+@app.get("/get-student/{student_id}")
+# Path adds a discription on the Fastapi frontend
+# Greater than, less than ,greater than/equal ...
+# gt, lt ,ge, le
+
+def get_student(student_id: int = Path(None, description="The id of the student",gt=0,lt=3)):
+    return students[student_id]
+
